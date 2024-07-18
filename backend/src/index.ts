16bfa9ebce6412f412fc9a95681579db9353cebd
Dinuka <dinukanh@gmail.com>
2024-07-19 09:54:10 +1000

    init fix (ts-node issues)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.get("/", async (req: Request, res: Response) => {
+    res.json({ message: "Hello" });
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server started on localhost:3000");
 });
