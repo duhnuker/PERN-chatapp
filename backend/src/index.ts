4dc59941ac9770bff12d6f7d5c51dcbb4e7c1513
Dinuka <dinukanh@gmail.com>
2024-07-19 18:13:39 +1000

    auth routes and controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req: Request, res: Response) => {
-    res.json({ message: "Hello" });
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(3000, () => {
     console.log("Server started on localhost:3000");
 });
+
