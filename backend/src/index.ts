cc778270c3c742261716293bbc83bdcfd6e59e04
duhnuker <dinukanh@gmail.com>
2024-07-24 12:59:38 +1000

    react routing, layout and styling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
-app.use(cookieParser());
+app.use(cookieParser()); 
 app.use(express.json());
 app.use(cors());
 
